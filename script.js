8e28a53738b448a91fb49188056007dc95aad1b5
rakeshgit <rakeshtagavula31@gmail.com>
2023-04-29 21:52:21 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,65 @@
+// function greeter()
+// {
+//     let today=new Date();
+//     console.log(today);
+// }
+// greeter();
+
+// let person=new Object();
+// {
+//     person.no=9704394209;
+//     person.name='Rakesh';
+//     person.location="Gurramguda";
+//     //person.Date();
+//     console.log(person);
+// }
+
+// let age = 22;
+    
+//     function ageVerify(){
+//     console.log(this.age);
+// }
+
+// ageVerify();    
+
+
+
+// let names=['Rakesh', 'Gouthami','Satyavathi', 'Srikanth Rao'];
+// let respectedNames=names.map(name => name +" " + 'Garu');
+// console.log(respectedNames);
+
+// function details(){
+//     let name= 'Rakesh';
+//     id= 224;
+//     phone= 9490672298;
+//     address= 'Gurramguda';
+// }
+// let person=new Object();
+
+// console.log(person.details());
+
+// let d=new Date();
+// console.log(d);
+
+// const child=document.querySelector('.child');
+// child.parentElement.style.border='2px solid red';
+
+const city=new arr['hyderabad','Delhi','pune','mumbai','chennai','Bangalore','Delhi','Delhi'];
+let count=0;
+for(int i=0;i<=arr.length;i++)
+{
+    if(count[i]>=2)
+    {
+        console.log(arr[i]);
+    }
+        count++;
+}
+
+
+
+
+
+// let output;
+// output=child;
+// console.log(child);
+
